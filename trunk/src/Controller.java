afeb77ce57482f99c23385cfd842ecf58e14f1bc
ben.trivett <ben.trivett@e9280bd6-9476-d34b-f39f-3bd00a32cacd>
2011-01-08 16:37:39 +0000

    Added empty model and controller.

---
+++ trunk/src/Controller.java
@@ -0,0 +1,8 @@
+/**
+ * @author Ben Trivett
+ *
+ */
+public class Controller {
+	// Contains event listeners for view GUI and the methods that control
+	// sending/receiving info from view to/from interpreter/loader.
+}
