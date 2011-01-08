73d63c743af6d930153a5273ccd61ec57cd8e938
ben.trivett <ben.trivett@e9280bd6-9476-d34b-f39f-3bd00a32cacd>
2011-01-08 16:37:39 +0000

    Added empty model and controller.

---
+++ src/Model.java
@@ -0,0 +1,9 @@
+
+/**
+ * @author #1Group
+ *
+ */
+public class Model {
+	// Use this class to store all the memory and register info in one spot
+	// since the loader, interpreter, and simulator all need access to it.
+}
