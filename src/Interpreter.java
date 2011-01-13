b50f176fee041cde9820ed7bd68bf9302de80570
dmglouis@gmail.com <dmglouis@gmail.com@e9280bd6-9476-d34b-f39f-3bd00a32cacd>
2011-01-13 20:47:13 +0000

    Interpreter. Will finish by friday evening

---
+++ src/Interpreter.java
@@ -0,0 +1,25 @@
+// CODE IN PROGRESS	
+
+	/**
+	 * This class interprets the instructions and executes them.
+	 * Will only stop execution if HALT instruction is encountered.
+	 * @author louisg
+	 *
+	 */
+	public class Interpreter {
+		/**
+		 * This array will hold the entire memory of the machine.
+		 * As such, it will have a length of 65,536. However, the
+		 * only locations with values in them will be the ones fed
+		 * in from the loader.
+		 */
+		public String[] memoryLength;
+		
+		/**
+		 * This integer will have the start value of the intructions
+		 * in hex (although it will actually be an integer)
+		 */
+		public int hexStartValue;
+		
+		public static void main(String[] args)
+	}
