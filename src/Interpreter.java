c704724f95615a7762c420d7cabb206711677ecf
dmglouis@gmail.com <dmglouis@gmail.com@e9280bd6-9476-d34b-f39f-3bd00a32cacd>
2011-01-15 06:17:24 +0000

    I have only completed the case select for selecting which instruction to use. From there onwards, I plan on making several separate classes to handle each case (or in some cases, combine them). Sorry for not completing as much as I said I would. I have a hectic drive and have only now been able to work on this.

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -3,23 +3,52 @@
 	/**
 	 * This class interprets the instructions and executes them.
 	 * Will only stop execution if HALT instruction is encountered.
-	 * @author louisg
+	 * @author GerardLouis
 	 *
 	 */
 	public class Interpreter {
 		/**
 		 * This array will hold the entire memory of the machine.
-		 * As such, it will have a length of 65,536. However, the
-		 * only locations with values in them will be the ones fed
-		 * in from the loader.
+		 * As such, it will have a length of 65,536.
 		 */
 		public String[] memoryLength;
 		
 		/**
-		 * This integer will have the start value of the intructions
+		 * This integer will have the start value of the instructions
 		 * in hex (although it will actually be an integer)
 		 */
 		public int hexStartValue;
+		/**
+		 * This integer will have the location in the array of the
+		 * next instruction.
+		 */
+		public int programCounter;
 		
-		public static void main(String[] args)
+		public instructionParser parserOfInstructions;
+		/**
+		 * This constructor initiates an Instructor object.
+		 * @param memory This String array will contain the list of
+		 * instructions to be executed 
+		 * @param hexStartValue This value will have the starting point 
+		 */
+		public Interpreter (
+			String[] memory,
+			int hexStartValue
+		) {
+			this.memoryLength = memory;
+			this.hexStartValue = hexStartValue;
+			this.programCounter = this.hexStartValue;
+			this.parserOfInstructions = new instructionParser();
+		}
+		
+		public void ExecuteAllInstructions() {
+			while (this.memoryLength[this.programCounter].charAt(0) != 'F' && 
+					this.memoryLength[this.programCounter].charAt(2) != '2' && 
+					this.memoryLength[this.programCounter].charAt(3) != '5') {
+				String instruction = this.memoryLength[this.programCounter];
+				// The line below should be uncommented once the parse method
+				// has been written.
+				//this.parserOfInstructions.parse(instruction);
+			}
+		}
 	}
