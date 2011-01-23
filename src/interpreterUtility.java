b6941af57ea97d808004167d1e3ab8de74011546
dmglouis@gmail.com <dmglouis@gmail.com@e9280bd6-9476-d34b-f39f-3bd00a32cacd>
2011-01-23 06:03:57 +0000

    Alright guys, I have completed everything I was supposed to. Basically, the parser works one step at a time and returns a String (in addition to modifying the String array for memory changes and Integer array for register changes). This string can be one of 5 possibilities:

--- src/interpreterUtility.java
+++ src/interpreterUtility.java
@@ -5,144 +5,7 @@
  *
  */
 public class interpreterUtility {
-	/**
-	 * Public static helper function to decode the instruction from the first
-	 * hex digit. It returns the English pseudo-code of the instruction
-	 * @param word The 4 digit hex String passed in from the interpreter.
-	 * @return English pseudo-code of the instruction in the word passed in.
-	 */
-	public static String decodeFirstHexDigit(
-			String word
-	) {
-		// A character firstDigit is assigned to the first character of
-		// the String word passed in. Also a String instruction is initialized
-		// to null.
-		char firstDigit = word.charAt(0);
-		String instruction = null;
-		
-		// Then a case select is used to determine which English instruction
-		// is to be used.
-		switch (firstDigit) {
-			case '0': instruction = "BRx";
-				break;
-			case '1': instruction = "ADD";
-				break;
-			case '2': instruction = "LD";
-				break;
-			case '3': instruction = "ST";
-				break;
-			case '4': instruction = "JSR";
-				break;
-			case '5': instruction = "AND";
-				break;
-			case '6': instruction = "LDR";
-				break;
-			case '7': instruction = "STR";
-				break;
-			case '8': instruction = "DBUG";
-				break;
-			case '9': instruction = "NOT";
-				break;
-			case 'A': instruction = "LDI";
-				break;
-			case 'B': instruction = "STI";
-				break;
-			case 'C': instruction = "JSRR";
-				break;
-			case 'D': instruction = "RET";
-				break;
-			case 'E': instruction = "LEA";
-				break;
-			case 'F': instruction = "TRAP";
-				break;
-			default: break;
-		}
-		// Finally English instruction is returned.
-		return instruction;
-	}
-/*
-	/**
-	 * Public static function that returns the base 10 equivalent of 
-	 * the digit specified, using the position specified in the integer
-	 * passed in.
-	 * @param word A String of four characters representing the hex 
-	 * value to be decoded.
-	 * @param hexCharacter A character which represents the digit in hex
-	 * @param position The position in the hex string the single digit
-	 * is in. For example if the entered digit is E and position 2, the
-	 * resultant integer will be 448.
-	 * @return An integer which is base 10 conversion of the passed in hex
-	 * digit, using the int position to calculate the power the final result
-	 * has to be multiplied by.
-	 */
-/*	
-	public static int decodeDigitMemoryLocation(
-			char hexCharacter,
-			int position
-	) {
-		// Sets int memoryLocation to zero.
-		int memoryLocation = 0;
-		
-		// If-else to check if the character passed in is a digit.
-		// If it is a digit, memoryLocation is set to the base-10
-		// version of that digit.
-		if (Character.isDigit(hexCharacter)) {
-			memoryLocation = Character.digit(hexCharacter, 10);
-		}
-		// Else, memoryLocation is set to 10, 11, 12, 13, 14 or 15,
-		// depending if the character is A, B, C, D, E, or F.
-		else {
-			if (hexCharacter == 'A') {
-				memoryLocation = 10;
-			}
-			else if (hexCharacter == 'B') {
-				memoryLocation = 11;
-			}
-			else if (hexCharacter == 'C') {
-				memoryLocation = 12;
-			}
-			else if (hexCharacter == 'D') {
-				memoryLocation = 13;
-			}
-			else if (hexCharacter == 'E') {
-				memoryLocation = 14;
-			}
-			else {
-				memoryLocation = 15;
-			}
-		}
-		
-		// Then, memoryLocation is multiplied by the exponent
-		// of 16 raised to the position integer passed in. That results in
-		// the final base 10 representation of the hex digit passed
-		// in. This result is returned.
-		return ((int)Math.pow(16, position))*memoryLocation;
-	}
 	
-	/**
-	 * This public static function decodes a String of hex characters
-	 * into a integer of base 10 value. While this function will work on
-	 * strings of length greater than four, the integer returned will point
-	 * to a location out of memory range.
-	 * @param word The string of hex characters to be converted to a base
-	 * 10 integer. The maximum length allowed is four.
-	 * @return An integer base 10 conversion of the hex characters in
-	 * the string that is passed in.
-	 */
-/*	
-	public static int decodeEntireMemoryLocation(
-			String word
-	) {
-		int counter = word.length();
-		int result = 0;
-		while (counter > 0) {
-			result += interpreterUtility.decodeDigitMemoryLocation(
-					word.charAt(counter-1), (word.length()-counter));
-			counter--;
-		}
-		return result;
-	}
-*/	
 	/**
 	 * Public static function to decode the last 3 digits (pos[0:2]) into
 	 * a String of binary numbers.
@@ -172,27 +35,79 @@ public class interpreterUtility {
 	public static int signExtendBinaryString(
 			String binaryString
 	) {
-		boolean negative = (binaryString.charAt(0) == '1');
-		StringBuffer tempString = new StringBuffer();
-		int counter = binaryString.length();
 		int result = 0;
-		while (counter > 1) {
-			while ((binaryString.charAt(counter-1)) != '1') {
-				tempString.insert(0, (binaryString.charAt(counter-1)));
+		if ((binaryString.charAt(0) == '1')) {
+			StringBuffer tempString = new StringBuffer();
+			int counter = binaryString.length();
+			while (counter > 1) {
+				while ((binaryString.charAt(counter-1)) != '1') {
+					tempString.insert(0, (binaryString.charAt(counter-1)));
+					counter--;
+				}
+				if (binaryString.charAt(counter-1) == '0') {
+					tempString.insert(0, '1');
+				}
+				else {
+					tempString.insert(0, '0');
+				}
 				counter--;
 			}
-			if (binaryString.charAt(counter-1) == '0') {
-				tempString.insert(0, '1');
-			}
-			else {
-				tempString.insert(0, '0');
-			}
-			counter--;
+			result = 0 - Integer.parseInt((tempString.toString()), 2);
 		}
-		result = Integer.parseInt((tempString.toString()), 2);
-		if (negative) {
-			result = 0-result;
+		else {
+			result = Integer.parseInt(binaryString, 2);
 		}
 		return result;
+	}
+	
+	/**
+	 * This function will check if the binary string passed is a valid
+	 * value for a register. It must parse to a value between 0 and 7, 
+	 * and so must have a maximum length of 3.
+	 * @param binaryRep The binary representation of the register number
+	 * being passed in.
+	 * @return True if the binary string passed in represents a decimal
+	 * number between 0 and 7 (inclusive).
+	 */
+	public static boolean isRegisterValue(
+			String binaryRep
+	) {
+		return (Utility.HexToDecimalValue(Utility.BinaryToHex(binaryRep)) < 8 &&
+				Utility.HexToDecimalValue(Utility.BinaryToHex(binaryRep)) >= 0);
+	}
+	
+	/**
+	 * Increments the given hex string by 1 and returns a new string
+	 * with the incremented value.
+	 * @param hex The hexadecimal string that is to be incremented.
+	 * @return A hex string that represents the hex string passed in,
+	 * incremented by 1.
+	 */
+	public static String IncrementHexValue(String hex) {
+		Integer result = Utility.HexToDecimalValue(hex);
+		result++;
+		return Integer.toHexString(result).toUpperCase();
+	}
+	
+	/**
+	 * Decrements the given hex string by 1 and returns a new string
+	 * with the decremented value.
+	 * @param hex The hexadecimal string that is to be decremented.
+	 * @return A hex string that represents the hex string passed in,
+	 * decremented by 1.
+	 */
+	public static String DecrementHexValue(String hex) {
+		Integer result = Utility.HexToDecimalValue(hex);
+		result--;
+		return Integer.toHexString(result).toUpperCase();
+	}
+	
+	public static boolean isOnSamePage(String operand, String programCounter) {
+		operand = Utility.HexToBinary(operand);
+		programCounter = Utility.HexToBinary(programCounter);
+		operand = operand.substring(0, 6);
+		programCounter = programCounter.substring(0, 6);
+		
+		return (Integer.parseInt(operand, 2) == Integer.parseInt(programCounter, 2));
 	}
 }
